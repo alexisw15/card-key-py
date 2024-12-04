74adc068315e028952eb66fd92a9696252a5f606
alexisw15 <W0426652@campus.nscc.ca>
2024-12-04 10:50:28 -0400

    ok i was an idiot

--- anomalyFinder.py
+++ anomalyFinder.py
@@ -1,20 +1,19 @@
-
 import csv
+print("test if it works")
 
 def main():
-    print("is this thing even on")
-    with open('logs.csv', newline='') as csvfile:
+    print("does this part of the program work")
+    
+    with open('logs.csv') as csvfile:
         csvData = list(csv.reader(csvfile, delimiter = '\t'))
-        
         header = csvData[0]
-       
         rows = csvData[1:]
-       
         employee_num_index = 0
         hour_in_index = 3
         hour_out_index = 7
         
         anomalies = []
+        
         for row in rows:
             try:
                 hourIn = int(row[hour_in_index])
@@ -27,8 +26,10 @@ def main():
     print("anomaly type 1: (clocked out at 0):   ")
     for anomaly in anomalies:
         print (anomaly)
+    else:
+        print("what about down here?")
     
-
+main()
 
 
 #def main():
