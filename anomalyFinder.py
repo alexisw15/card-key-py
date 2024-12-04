29ec58c5d33c0d6521a6d687120d06973caf5a7a
alexisw15 <W0426652@campus.nscc.ca>
2024-12-04 13:13:02 -0400

    added more comments still a W

--- anomalyFinder.py
+++ anomalyFinder.py
@@ -1,8 +1,7 @@
-import csv
-print("test if it works")
+import csv #needed to process the csv file.
 
 def main():
-    print("anomaly finder v1")
+    print("anomaly finder v1") 
     #open CSV and tell it that it uses tabs and not commas
     with open('logs.csv') as csvfile:
         csvData = list(csv.reader(csvfile, delimiter = '\t'))
@@ -19,26 +18,26 @@ def main():
         #create list for anomalies
         anomalies = []
         
-        for row in rows:
+        for row in rows: #go through all the rows that arent the header
             try:
                 hourIn = int(row[hour_in_index])
                 hourOut = int(row[hour_out_index])
                 minIn = int(row[min_in_index])
                 minOut = int(row[min_out_index])
                 empNum = row[employee_num_index].strip()
-
                 #make these into numbers that are easier to call and print out
-                
-                if hourIn == 0 or hourOut == 0:
+                # strip on empNum means that we remove the whitespace for cleaner output
+                if hourIn == 0 or hourOut == 0: #check for anomalies in clock in/out times. It's not normal to clock in or out at 0:0.
                     anomalies.append((empNum, hourIn, minIn, hourOut, minOut))
+                    # adds these values to our list of anomalies
             except ValueError:
                 print("error: stupid idiot disease detected!")
-                        #just in case. you never know.
-    
-    print("anomaly type 1: (clocked out at 0):  \n ")
+                        #have to have error handling just in case.
+    #outputting the detected anomalies
+    print("anomaly type 1: (clocked in/out at 0):  \n ") #named it type 1 because i thought there would be others.
     for i, anomaly in enumerate(anomalies, start=1):
         empNum, hourIn, minIn, hourOut, minOut = anomaly
-        print(f"Anomaly {i}: Employee {empNum} - Time in: {hourIn}:{minIn} - Time out: {hourOut}:{minOut} ")
+        print(f"Anomaly {i}: Employee {empNum} - Time in: {hourIn}:{minIn} - Time out: {hourOut}:{minOut} ") #outputs formatted entry for each anomaly.
     else:
         print("Done.")
     
